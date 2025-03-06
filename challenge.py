8037ab2daeaf3fb494cb14427a52fe2976378599
vkuoppala <ville.a.kuoppala@seamk.fi>
2025-03-06 12:03:48 +0200

    Filtteröinnille oma funktio
    Filtteröinti on siirretty omaan funktioon, joka mahdollistaa funktioita kasaavien funktioiden siistimisen. Siirretty tiedostoja omiin kansioihin

--- challenge.py
+++ challenge.py
@@ -15,7 +15,7 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 @task
 def challenge():
     """Invoice extraction challenge."""
-    TULOS_FILE = "output.csv"
+    TULOS_FILE = "output/output.csv"
     WEBSITE = "https://rpachallengeocr.azurewebsites.net/"
     clear_csv_file(TULOS_FILE)
     open_website(WEBSITE)
@@ -62,24 +62,29 @@ def get_table_data(soup):
     return soup.find_all("tr", class_=["odd", "even"])
 
 def information_handler(page_data, tulos_file, website):  
+    good_data = drop_unwanted_rows(page_data)
+    if good_data is None:
+        return
+    for row in good_data:
+        cells = row.find_all("td")
+        picture = download_invoice(cells[3].find("a")["href"], cells[0].text.strip(), website)
+        text = extract_data_from_picture(picture)
+        invoice_number, invoice_date, company_name, total_due = find_keywords(text)
+        write_to_csv_file(cells[1].text.strip(), cells[2].text.strip(), invoice_number, invoice_date, company_name, total_due, tulos_file)
+
+def drop_unwanted_rows(page_data):
+    good_files = []
     for table in page_data:
         for row in table:
             cells = row.find_all("td")
             if len(cells) < 4:
                 continue
-            
-            invoice_number_counter = cells[0].text.strip()
-            invoice_id = cells[1].text.strip()
             due_date_str = cells[2].text.strip()
             due_date = datetime.strptime(due_date_str, "%d-%m-%Y")
             if due_date > datetime.today():
                 continue
-
-            link = cells[3].find("a")["href"]
-            picture = download_invoice(link, invoice_number_counter, website)
-            text = extract_data_from_picture(picture)
-            invoice_number, invoice_date, company_name, total_due = find_keywords(text)
-            write_to_csv_file(invoice_id, due_date_str, invoice_number, invoice_date, company_name, total_due, tulos_file)
+            good_files.append(row)
+    return good_files
 
 def download_invoice(href, invoice_number_value, website):
     url = f"{website}{href}"
