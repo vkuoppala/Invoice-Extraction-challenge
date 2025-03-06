9c435e6e996c379d398bdf97a0cc9fb735cb0c09
vkuoppala <ville.a.kuoppala@seamk.fi>
2025-03-06 13:23:06 +0200

    Added config file, made umbrella function more umbrella like
    Added config file to store constants.
    Umbrella file no longer handles data in a major way, instead it just calls functions to do so, acting more of a hub.

--- challenge.py
+++ challenge.py
@@ -9,32 +9,30 @@ import re
 import csv
 from datetime import datetime
 from company import find_keywords
+from _config import TULOS_FILE, WEBSITE, TESSERACT_CMD
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
+pytesseract.pytesseract.tesseract_cmd = TESSERACT_CMD
 @task
 def challenge():
     """Invoice extraction challenge."""
-    TULOS_FILE = "output/output.csv"
-    WEBSITE = "https://rpachallengeocr.azurewebsites.net/"
-    clear_csv_file(TULOS_FILE)
-    open_website(WEBSITE)
+    clear_csv_file()
+    open_website()
     start()
     sleep(0.05)
     page_data = gather_page_data()
-    information_handler(page_data, TULOS_FILE, WEBSITE)
-    submit(TULOS_FILE)
+    information_handler(page_data)
+    submit()
     sleep(5)
 
-def clear_csv_file(file_path):
+def clear_csv_file():
     """Clear the contents of the CSV file."""
-    with open(file_path, 'w', newline="") as csvfile:
+    with open(TULOS_FILE, 'w', newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(["ID", "DueDate", "InvoiceNo", "InvoiceDate", "CompanyName", "TotalDue"])
 
-def open_website(url):
+def open_website():
     """Open the challenge website."""
-    browser.goto(url)
+    browser.goto(WEBSITE)
 
 def start():
     """Presses start button."""
@@ -71,17 +69,13 @@ def gather_page_data():
         page_data.append(get_table_data(data))
     return page_data
 
-def information_handler(page_data, tulos_file, website):  
+def information_handler(page_data):  
     """Acts as a umbrella function for the data extraction and writing to CSV."""
     good_data = drop_unwanted_rows(page_data)
     if good_data is None:
         return
-    for row in good_data:
-        cells = row.find_all("td")
-        picture = download_invoice(cells[3].find("a")["href"], cells[0].text.strip(), website)
-        text = extract_data_from_picture(picture)
-        invoice_number, invoice_date, company_name, total_due = find_keywords(text)
-        write_to_csv_file(cells[1].text.strip(), cells[2].text.strip(), invoice_number, invoice_date, company_name, total_due, tulos_file)
+    extracted_data = extract_data(good_data)
+    write_to_csv_file(extracted_data)
 
 def drop_unwanted_rows(page_data):
     """Drops rows that are not needed to process."""
@@ -98,10 +92,27 @@ def drop_unwanted_rows(page_data):
             good_files.append(row)
     return good_files
 
-def download_invoice(href, invoice_number_value, website):
+def extract_data(data):
+    """Extracts relevant data from the tables."""
+    relevant_data = []
+    for row in data:
+        cells = row.find_all("td")
+        picture = download_invoice(cells[3].find("a")["href"], cells[0].text.strip())
+        if picture is None:
+            continue
+        text = extract_data_from_picture(picture)
+        if text is None:
+            continue
+        invoice_number, invoice_date, company_name, total_due = find_keywords(text)
+        relevant_data.append ((cells[1].text.strip(), cells[2].text.strip(), invoice_number, invoice_date, company_name, total_due))
+    return relevant_data
+
+def download_invoice(href, invoice_number_value):
     """Downloads the invoice document."""
-    url = f"{website}{href}"
+    url = f"{WEBSITE}{href}"
     response = requests.get(url)
+    if response.status_code != 200:
+        return None
     picture = f"output/screenshots/{invoice_number_value}.png"
     with open(picture, "wb") as file:
         file.write(response.content)
@@ -110,16 +121,18 @@ def download_invoice(href, invoice_number_value, website):
 def extract_data_from_picture(picture):
     """Extracts the text from the invoice document."""
     img = cv2.imread(picture)
+    if not img:
+        return None
     text = pytesseract.image_to_string(img)
     return text
     
-def write_to_csv_file(invoice_id, due_date_str, invoice_number, invoice_date, company_name, total_due, tulos_file):
+def write_to_csv_file(invoice_id, due_date_str, invoice_number, invoice_date, company_name, total_due):
     """Writes the extracted data to the CSV file."""
-    with open(tulos_file, "a", newline="") as csvfile:
+    with open(TULOS_FILE, "a", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow([invoice_id, due_date_str, invoice_number, invoice_date, company_name, total_due])
 
-def submit(file_path):
+def submit():
     """Submits the CSV file."""
     page = browser.page()
-    page.locator(selector="input[type='file']").set_input_files(file_path)
+    page.locator(selector="input[type='file']").set_input_files(TULOS_FILE)
